1ff53a92cd9ddefd3bb21bb5dace40e65b825e7e
kelvinkyalo <kelvinchax@gmail.com>
2020-02-24 09:51:30 +0300

    modify jQuery by adding different Classes for toggle effect"

--- IP-WEEK3/js/script.js
+++ IP-WEEK3/js/script.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//     $(".clickable").click(function() {
-//         $("#initially-showing").toggle();
-//         $("#initially-hidden").toggle();
-//     });
-// });
 $(document).ready(function() {
     $(".clickable").click(function() {
         $("#hidden").toggle();
