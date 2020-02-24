7577e992945020a11ab184e21bb528ab79c79225
kelvinkyalo <kelvinchax@gmail.com>
2020-02-24 11:23:31 +0300

    add a white box to my hover images

--- jq-simple-effects/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".clickable").click(function() {
-        $("#initially-showing").toggle();
-        $("#initially-hidden").toggle();
-    });
-});
--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-var substract = function(number1, number2) {
-    return number1 - number2;
-};
-alert(substract(10, 5));
+$(document).ready(function() {
+    $(".clickable").click(function() {
+        $("#hidden").toggle();
+        $("#showing").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".clickable2").click(function() {
+        $("#hidden2").toggle();
+        $("#showing2").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".clickable3").click(function() {
+        $("#hidden3").toggle();
+        $("#showing3").toggle();
+    });
+});
